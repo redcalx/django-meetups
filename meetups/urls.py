7ca44e3b8c5b49667694e069c72ba1bd231fb6ce
redcalx <lorenzo.leosb@gmail.com>
2023-08-19 19:20:29 -0300

    Initial commit

---
+++ meetups/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='all-meetups'),
+    path('<slug:meetup_slug>/success', views.registration_success, name='registration-success'),
+    path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
+]
